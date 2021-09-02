269dbb2d9bb8041019ae69f189f04bb488f321cc
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-02 13:43:06 +0200

    axios-on

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-console.log('Vue', Vue);
+console.log("Vue", Vue);
 
 Vue.config.devtools = true;
 
 const app = new Vue({
-  el: '#app',
-  data: {},
-  methods: {},
+	el: "#app",
+	data: {
+		songs: [],
+		error: false,
+	},
+	methods: {},
+	created() {
+		axios
+			.get("https://flynn.boolean.careers/exercises/api/array/music")
+			.then((res) => {
+				const songs = res.data.response;
+				this.songs = songs;
+			})
+			.catch(() => {
+				this.error = true;
+			});
+	},
 });
